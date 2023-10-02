5bd83b71aa776e71385fa73207edd2879da7ec5a
pomy <121321457+yuliialesiuk@users.noreply.github.com>
2023-10-02 23:19:23 +0300

    add some imgs and modify code. add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,67 @@
-var gulp = require ("gulp");
-//додаткові плагіни Gulp
-var sass = require ("gulp-sass"), //конвертує SASS в CSS
-    cssnano = require ("gulp-cssnano"), //мінімізація CSS
-    autoprefixer = require ('gulp-autoprefixer'), //додавання префіксів в
-    //CSS для підтримки
-    //старих браузерів
-    imagemin = require ('gulp-imagemin'), //стиснення зображень
-    concat = require ("gulp-concat"), //об'єднання файлів - конкатенація
-    uglify = require ("gulp-uglify"), //мінімізація javascript
-    rename = require ("gulp-rename"); //перейменування файлів
-//копіювання HTML файлів в папку dist
-gulp.task ( "html", function () {
-    return gulp.src ( "src / *. html")
-        .pipe (gulp.dest ( "dist"));
-});
-//об'єднання, компіляція Sass в CSS, додавання префіксів і подальша
-мінімізація коду
-gulp.task ( "sass", function () {
-    return gulp.src ( "src / sass / *. sass")
-        .pipe (concat ( 'styles.sass'))
-        .pipe (sass ())
-        .pipe (autoprefixer ({
-            browsers: [ 'last 2 versions'],
-            cascade: false
-        }))
-        .pipe (cssnano ())
-        .pipe (rename ({suffix: '.min'}))
-        .pipe (gulp.dest ( "dist / css"));
-});
-//об'єднання і стиснення JS-файлів
-gulp.task ( "scripts", function () {
-    return gulp.src ( "src / js / *. js") //вихідна директорія файлів
-        .pipe (concat ( 'scripts.js')) // конкатенація js-файлів в один
-        .pipe (uglify ()) //стиснення коду
-        .pipe (rename ({suffix: '.min'})) //перейменування файлу з
-        //приставкою .min
-        .pipe (gulp.dest ( "dist / js")); // директорія продакшена
-});
-//cтискання зображень
-gulp.task ( 'imgs', function () {
-    return gulp.src ( "src / images /*.+ (jpg | jpeg | png | gif)")
-        .pipe (imagemin ({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            interlaced: true
-        }))
-        .pipe (gulp.dest ( "dist / images"))
-});
-//відстежування за змінами у файлах
-gulp.task ( "watch", function () {
-    gulp.watch ( "src / *. html", [ "html"]);
-    gulp.watch ( "src / js / *. js", [ "scripts"]);
-    gulp.watch ( "src / sass / *. sass", [ "sass"]);
-    gulp.watch ( "src / images /*.+ (jpg | jpeg | png | gif)", [ "imgs"]);
-});
-//Запуск тасків за замовчуванням
-gulp.task ( "default", [ "html", "sass", "scripts", "imgs", "watch"]);
-var plumber = require('gulp-plumber');
-var coffee = require('gulp-coffee');
-
-gulp.src('./src/*.ext')
-    .pipe(plumber())
-    .pipe(coffee())
-    .pipe(gulp.dest('./dist'));
-const gulp = require('gulp');
-const svgSprite = require('gulp-svg-sprite');
-
-gulp.src('path/to/assets/*.svg')
-    .pipe(svgSprite(/* ... Insert your configuration here ... */))
-    .pipe(gulp.dest('out'));
-var gulp        = require('gulp');
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require('sass'));
+var cssnano = require("gulp-cssnano");
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
 var browserSync = require('browser-sync').create();
 
-// Static server
-gulp.task('browser-sync', function() {
+gulp.task("html", function () {
+    return gulp.src("app/*.html")
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("css", function () {
+    return gulp.src("app/css/*.css")
+        .pipe(concat('styles.css'))
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("sass", function () {
+    return gulp.src("app/sass/*.sass")
+        .pipe(concat('styles.sass'))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("scripts", function () {
+    return gulp.src("app/js/*.js")
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('imgs', function () {
+    return gulp.src("app/img/*.{jpg,jpeg,png,gif}")
+        .pipe(gulp.dest("dist/images"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task("watch", function () {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: ["./app", "./dist"]
         }
     });
+    gulp.watch("app/*.html", gulp.series("html"));
+    gulp.watch("app/js/*.js", gulp.series("scripts"));
+    gulp.watch("app/sass/*.sass", gulp.series("sass"));
+    gulp.watch("app/img/*.{jpg,jpeg,png,gif}", gulp.series("imgs"))
+    gulp.watch("dist").on('change', browserSync.reload);
 });
 
-// or...
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
-});
+gulp.task("default", gulp.series("html", "sass", "css", "scripts", "imgs", "watch"));
