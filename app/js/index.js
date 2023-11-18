d5351e1fb37ac336658cc6d82055c7dc73c3f39c
pomy <121321457+yuliialesiuk@users.noreply.github.com>
2023-11-18 14:34:03 +0200

    ****

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,22 @@
-alert('x');
-alert('x');
-alert('x');
-alert('x');
-alert('x');
-//shift+alt+a
-//ctrl+/
+// <block:setup:1>
+
+const ctx = document.getElementById('statics');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
